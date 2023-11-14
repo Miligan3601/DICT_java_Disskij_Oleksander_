d3b6ccb2b679e93159ce71bfde8139d4a55d268b
Alexander <o.y.dyskyi@student.khai.edu>
2023-11-14 22:52:01 +0200

    Hangman branch created two

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -3,8 +3,19 @@ package Hangman;
 import java.util.Scanner;
 public class hangman {
     public static void main(String[] args) {
-        System.out.println("HANGMAN");
-        System.out.println("The game will be available soon.");
+        Scanner scanner = new Scanner(System.in);
+        String secretWord = "java";
 
+        System.out.println("HANGMAN");
+        System.out.print("Guess the word: > ");
+        String userInput = scanner.nextLine();
+
+        if (userInput.equalsIgnoreCase(secretWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+
+        scanner.close();
     }
 }
