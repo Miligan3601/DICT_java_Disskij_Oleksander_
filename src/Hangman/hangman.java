34f0ef89837212bce70a0c2559015421aedc0d17
Alexander <o.y.dyskyi@student.khai.edu>
2023-11-14 22:56:22 +0200

    Hangman branch created four

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -4,22 +4,28 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class hangman {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] wordList = {"python", "java", "javascript", "kotlin"};
-        Random random = new Random();
-        String secretWord = wordList[random.nextInt(wordList.length)];
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            String[] wordList = {"python", "java", "javascript", "kotlin"};
+            Random random = new Random();
+            String secretWord = wordList[random.nextInt(wordList.length)];
 
-        System.out.println("HANGMAN");
-        System.out.print("Guess the word: > ");
-        String userInput = scanner.nextLine();
+            StringBuilder displayWord = new StringBuilder();
+            displayWord.append(secretWord.charAt(0)).append(secretWord.charAt(1));
+            for (int i = 2; i < secretWord.length(); i++) {
+                displayWord.append("-");
+            }
 
-        if (userInput.equalsIgnoreCase(secretWord)) {
-            System.out.println("You survived!");
-        } else {
-            System.out.println("You lost!");
+            System.out.println("HANGMAN");
+            System.out.print("Guess the word " + displayWord + ": > ");
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase(secretWord)) {
+                System.out.println("You survived!");
+            } else {
+                System.out.println("You lost!");
+            }
+
+            scanner.close();
         }
-
-        scanner.close();
     }
-}
