1fad2cfcc2145398e45b2d1f7d41204ae7fba4c7
Alexander <o.y.dyskyi@student.khai.edu>
2023-11-14 22:47:47 +0200

    Hangman branch created one

---
+++ src/Hangman/hangman.java
@@ -0,0 +1,10 @@
+package Hangman;
+
+import java.util.Scanner;
+public class hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN");
+        System.out.println("The game will be available soon.");
+
+    }
+}
