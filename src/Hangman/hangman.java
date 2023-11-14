76d5148bd5eaad82a3a2a4d247b3bd02cebc914e
Alexander <o.y.dyskyi@student.khai.edu>
2023-11-14 22:58:20 +0200

    Hangman branch created five

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -4,28 +4,60 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class hangman {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            String[] wordList = {"python", "java", "javascript", "kotlin"};
-            Random random = new Random();
-            String secretWord = wordList[random.nextInt(wordList.length)];
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] wordList = {"python", "java", "javascript", "kotlin"};
+        Random random = new Random();
+        String secretWord = wordList[random.nextInt(wordList.length)];
 
-            StringBuilder displayWord = new StringBuilder();
-            displayWord.append(secretWord.charAt(0)).append(secretWord.charAt(1));
-            for (int i = 2; i < secretWord.length(); i++) {
-                displayWord.append("-");
+        int attemptsLeft = 8;
+        boolean[] guessedLetters = new boolean[secretWord.length()];
+        boolean wordGuessed = false;
+
+        System.out.println("HANGMAN");
+        while (attemptsLeft > 0 && !wordGuessed) {
+            boolean letterGuessed = false;
+            System.out.print(displayWord(secretWord, guessedLetters) + "\nInput a letter: > ");
+            String userInput = scanner.nextLine().toLowerCase();
+
+            char userChar = userInput.charAt(0);
+            for (int i = 0; i < secretWord.length(); i++) {
+                if (secretWord.charAt(i) == userChar && !guessedLetters[i]) {
+                    guessedLetters[i] = true;
+                    letterGuessed = true;
+                }
             }
 
-            System.out.println("HANGMAN");
-            System.out.print("Guess the word " + displayWord + ": > ");
-            String userInput = scanner.nextLine();
-
-            if (userInput.equalsIgnoreCase(secretWord)) {
-                System.out.println("You survived!");
-            } else {
-                System.out.println("You lost!");
+            if (!letterGuessed) {
+                attemptsLeft--;
+                System.out.println("That letter doesn't appear in the word");
             }
 
-            scanner.close();
+            if (displayWord(secretWord, guessedLetters).equals(secretWord)) {
+                wordGuessed = true;
+            }
         }
+
+        if (wordGuessed) {
+            System.out.println("Thanks for playing!");
+            System.out.println("We'll see how well you did in the next stage");
+        } else {
+            System.out.println("You ran out of attempts!");
+            System.out.println("The word was: " + secretWord);
+        }
+
+        scanner.close();
     }
+
+    private static String displayWord(String secretWord, boolean[] guessedLetters) {
+        StringBuilder display = new StringBuilder();
+        for (int i = 0; i < secretWord.length(); i++) {
+            if (guessedLetters[i]) {
+                display.append(secretWord.charAt(i));
+            } else {
+                display.append("-");
+            }
+        }
+        return display.toString();
+    }
+}
