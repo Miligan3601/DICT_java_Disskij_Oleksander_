64bb090b1d24e6eed698ce2697c88bc27b1c3a1e
Alexander <o.y.dyskyi@student.khai.edu>
2023-12-06 19:32:35 +0200

    commit four

--- src/MatrixProcessing/MatrixProcessing.java
+++ src/MatrixProcessing/MatrixProcessing.java
@@ -3,91 +3,161 @@ import java.util.Scanner;
 public class MatrixProcessing {
     
     public static void main(String[] args) {
-        
-        Scanner scan = new Scanner(System.in);
-        
-        while (true) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
             System.out.println("1. Add matrices");
-            System.out.println("2. Multiply matrix by a constant"); 
+            System.out.println("2. Multiply matrix by a constant");
             System.out.println("3. Multiply matrices");
+            System.out.println("4. Transpose matrix");
             System.out.println("0. Exit");
-            
-            System.out.print("Your choice: ");
-            int choice = scan.nextInt();
-            
-            if(choice == 0) {
-                break;
-            }
-            
-            switch(choice) {
+            System.out.print("Your choice: > ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
                 case 1:
-                    addMatrices(scan);
+                    // Додайте код для додавання матриць
                     break;
-                case 2: 
-                    multiplyMatrixConstant(scan);
+                case 2:
+                    // Додайте код для множення матриці на константу
                     break;
                 case 3:
-                    multiplyMatrices(scan);
+                    // Додайте код для множення матриць
+                    break;
+                case 4:
+                    transposeMenu(scanner);
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
                     break;
                 default:
-                    System.out.println("Invalid choice");
+                    System.out.println("Invalid choice. Please enter a valid option.");
             }
-        }
+        } while (choice != 0);
         
-        scan.close();
-        
-    }
-    
-    public static void addMatrices(Scanner scan) {
-        System.out.print("Enter size of first matrix: ");
-        int r1 = scan.nextInt(); 
-        int c1 = scan.nextInt();
-        
-        System.out.print("Enter size of second matrix: ");
-        int r2 = scan.nextInt();
-        int c2 = scan.nextInt();
-        
-        if(r1 != r2 || c1 != c2) {
-            System.out.println("The operation cannot be performed.");
-            return;
-        }
-        
-        int[][] matrix1 = readMatrix(scan, r1, c1); 
-        int[][] matrix2 = readMatrix(scan, r2, c2);
-        
-        
-        int[][] result = addMatrices(matrix1, matrix2);
-        printMatrix(result);
+        scanner.close();
     }
 
-    public static int[][] addMatrices(int[][] matrix1, int[][] matrix2) {
-        int r = matrix1.length;
-        int c = matrix1[0].length;
-        
-        int[][] result = new int[r][c];
-        
-        for(int i=0; i<r; i++) {
-            for(int j=0; j<c; j++) {
-                result[i][j] = matrix1[i][j] + matrix2[i][j]; 
+    // Метод для виведення матриці на екран
+    private static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
             }
-        }        
-        return result;
+            System.out.println();
+        }
     }
-    
-    
-    public static void multiplyMatrixConstant(Scanner scan) {
+
+    // Метод для транспонування матриці за головною діагоналлю
+    private static void transposeMainDiagonal(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transposedMatrix = new int[cols][rows];
+
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                transposedMatrix[i][j] = matrix[j][i];
+            }
+        }
+
+        System.out.println("The result is:");
+        printMatrix(transposedMatrix);
     }
-    
-    public static void multiplyMatrices(Scanner scan) {
+
+    // Метод для транспонування матриці за побічною діагоналлю
+    private static void transposeSideDiagonal(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transposedMatrix = new int[cols][rows];
+
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                transposedMatrix[cols - 1 - i][rows - 1 - j] = matrix[j][i];
+            }
+        }
+
+        System.out.println("The result is:");
+        printMatrix(transposedMatrix);
     }
-    
-    public static int[][] readMatrix(Scanner scan, int r, int c) {
-        int[][] matrix = new int[r][c];  
+
+    // Метод для транспонування матриці за вертикаллю
+    private static void transposeVertical(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transposedMatrix = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposedMatrix[i][cols - 1 - j] = matrix[i][j];
+            }
+        }
+
+        System.out.println("The result is:");
+        printMatrix(transposedMatrix);
+    }
+
+    // Метод для транспонування матриці за горизонталлю
+    private static void transposeHorizontal(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transposedMatrix = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposedMatrix[rows - 1 - i][j] = matrix[i][j];
+            }
+        }
+
+        System.out.println("The result is:");
+        printMatrix(transposedMatrix);
+    }
+
+    // Метод для вибору типу транспонування
+    private static void transposeMenu(Scanner scanner) {
+        int choice;
+
+        do {
+            System.out.println("1. Main diagonal");
+            System.out.println("2. Side diagonal");
+            System.out.println("3. Vertical line");
+            System.out.println("4. Horizontal line");
+            System.out.print("Your choice: > ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    transposeMainDiagonal(readMatrix(scanner));
+                    break;
+                case 2:
+                    transposeSideDiagonal(readMatrix(scanner));
+                    break;
+                case 3:
+                    transposeVertical(readMatrix(scanner));
+                    break;
+                case 4:
+                    transposeHorizontal(readMatrix(scanner));
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option.");
+            }
+        } while (choice < 1 || choice > 4);
+    }
+
+    // Метод для зчитування матриці з консолі
+    private static int[][] readMatrix(Scanner scanner) {
+        System.out.print("Enter matrix size: > ");
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
         return matrix;
     }
-    
-    public static void printMatrix(int[][] matrix) {
-        
-    }
-    
 }
