247f366d84eca6b49a408b81e8bb8bb4865761bb
Alexander <o.y.dyskyi@student.khai.edu>
2023-12-06 19:26:23 +0200

    commit three

--- src/MatrixProcessing/MatrixProcessing.java
+++ src/MatrixProcessing/MatrixProcessing.java
@@ -3,60 +3,91 @@ import java.util.Scanner;
 public class MatrixProcessing {
     
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Зчитуємо розміри матриці
-        int rows = scanner.nextInt();
-        int cols = scanner.nextInt();
-
-        // Зчитуємо матрицю
-        int[][] matrix = readMatrix(scanner, rows, cols);
-
-        // Зчитуємо константу
-        int constant = scanner.nextInt();
-
-        // Множимо матрицю на константу
-        int[][] resultMatrix = multiplyByConstant(matrix, constant);
-
-        // Виводимо результат множення
-        printMatrix(resultMatrix);
-    }
-
-    // Метод для зчитування матриці з введення
-    private static int[][] readMatrix(Scanner scanner, int rows, int cols) {
-        int[][] matrix = new int[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                matrix[i][j] = scanner.nextInt();
+        
+        Scanner scan = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("1. Add matrices");
+            System.out.println("2. Multiply matrix by a constant"); 
+            System.out.println("3. Multiply matrices");
+            System.out.println("0. Exit");
+            
+            System.out.print("Your choice: ");
+            int choice = scan.nextInt();
+            
+            if(choice == 0) {
+                break;
+            }
+            
+            switch(choice) {
+                case 1:
+                    addMatrices(scan);
+                    break;
+                case 2: 
+                    multiplyMatrixConstant(scan);
+                    break;
+                case 3:
+                    multiplyMatrices(scan);
+                    break;
+                default:
+                    System.out.println("Invalid choice");
             }
         }
+        
+        scan.close();
+        
+    }
+    
+    public static void addMatrices(Scanner scan) {
+        System.out.print("Enter size of first matrix: ");
+        int r1 = scan.nextInt(); 
+        int c1 = scan.nextInt();
+        
+        System.out.print("Enter size of second matrix: ");
+        int r2 = scan.nextInt();
+        int c2 = scan.nextInt();
+        
+        if(r1 != r2 || c1 != c2) {
+            System.out.println("The operation cannot be performed.");
+            return;
+        }
+        
+        int[][] matrix1 = readMatrix(scan, r1, c1); 
+        int[][] matrix2 = readMatrix(scan, r2, c2);
+        
+        
+        int[][] result = addMatrices(matrix1, matrix2);
+        printMatrix(result);
+    }
+
+    public static int[][] addMatrices(int[][] matrix1, int[][] matrix2) {
+        int r = matrix1.length;
+        int c = matrix1[0].length;
+        
+        int[][] result = new int[r][c];
+        
+        for(int i=0; i<r; i++) {
+            for(int j=0; j<c; j++) {
+                result[i][j] = matrix1[i][j] + matrix2[i][j]; 
+            }
+        }        
+        return result;
+    }
+    
+    
+    public static void multiplyMatrixConstant(Scanner scan) {
+    }
+    
+    public static void multiplyMatrices(Scanner scan) {
+    }
+    
+    public static int[][] readMatrix(Scanner scan, int r, int c) {
+        int[][] matrix = new int[r][c];  
         return matrix;
     }
-
-    // Метод для множення матриці на константу
-    private static int[][] multiplyByConstant(int[][] matrix, int constant) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int[][] resultMatrix = new int[rows][cols];
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                resultMatrix[i][j] = matrix[i][j] * constant;
-            }
-        }
-        return resultMatrix;
+    
+    public static void printMatrix(int[][] matrix) {
+        
     }
-
-    // Метод для виведення матриці
-    private static void printMatrix(int[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
+    
 }
