aa01e55ab0303288aed6ee20961bc26762e16341
Alexander <o.y.dyskyi@student.khai.edu>
2023-12-14 12:54:46 +0200

    commit three

--- src/CreditCalculator/CreditCalculator.java
+++ src/CreditCalculator/CreditCalculator.java
@@ -4,29 +4,59 @@ public class CreditCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the loan principal:");
-        double principal = Double.parseDouble(scanner.nextLine());
-
         System.out.println("What do you want to calculate?");
-        System.out.println("type 'm' – for number of monthly payments,");
-        System.out.println("type 'p' – for the monthly payment:");
+        System.out.println("type 'n' for number of monthly payments,");
+        System.out.println("type 'a' for annuity monthly payment amount,");
+        System.out.println("type 'p' for loan principal:");
         String calculationType = scanner.nextLine();
 
-        double payment;
-        double periods;
+        double principal = 0;
+        double payment = 0;
+        double periods = 0;
+        double interest = 0;
 
-        if (calculationType.equals("m")) {
-            System.out.println("Enter the monthly payment:");
-            payment = Double.parseDouble(scanner.nextLine());
-            periods = Math.ceil(principal / payment);
-            System.out.println("It will take " + (int) periods + " months to repay the loan");
-        } else if (calculationType.equals("p")) {
-            System.out.println("Enter the number of months:");
-            periods = Double.parseDouble(scanner.nextLine());
-            payment = Math.ceil(principal / periods);
-            double lastPayment = principal - (periods - 1) * payment;
-            System.out.println("Your monthly payment = " + (int) payment +
-                    " and the last payment = " + (int) lastPayment);
+        switch (calculationType) {
+            case "n":
+                System.out.println("Enter the loan principal:");
+                principal = Double.parseDouble(scanner.nextLine());
+                System.out.println("Enter the monthly payment:");
+                payment = Double.parseDouble(scanner.nextLine());
+                System.out.println("Enter the loan interest:");
+                interest = Double.parseDouble(scanner.nextLine());
+
+                double i = interest / (12 * 100);
+                periods = Math.log(payment / (payment - i * principal)) / Math.log(1 + i);
+                int years = (int) periods / 12;
+                int months = (int) periods % 12;
+                System.out.println("It will take " + years + " years and " + months + " months to repay this loan!");
+                break;
+            case "a":
+                System.out.println("Enter the loan principal:");
+                principal = Double.parseDouble(scanner.nextLine());
+                System.out.println("Enter the number of periods:");
+                periods = Double.parseDouble(scanner.nextLine());
+                System.out.println("Enter the loan interest:");
+                interest = Double.parseDouble(scanner.nextLine());
+
+                i = interest / (12 * 100);
+                payment = principal * (i * Math.pow(1 + i, periods)) / (Math.pow(1 + i, periods) - 1);
+                System.out.println("Your monthly payment = " + (int) payment + "!");
+                break;
+            case "p":
+                System.out.println("Enter the annuity payment:");
+                payment = Double.parseDouble(scanner.nextLine());
+                System.out.println("Enter the number of periods:");
+                periods = Double.parseDouble(scanner.nextLine());
+                System.out.println("Enter the loan interest:");
+                interest = Double.parseDouble(scanner.nextLine());
+
+                i = interest / (12 * 100);
+                principal = payment * (Math.pow(1 + i, periods) - 1) / (i * Math.pow(1 + i, periods));
+                System.out.println("Your loan principal = " + (int) principal + "!");
+                break;
+            default:
+                System.out.println("Invalid calculation type!");
+                break;
         }
 
         scanner.close();
